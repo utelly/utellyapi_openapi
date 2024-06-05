5b45adf3df1e9c83b02fb70a599e99ca0a153e55
utelly <stoplight@utelly.com>
2024-06-05 11:40:45 +0100

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNw/spectral.js?branch_slug=master&token=f4ecd5c6-e606-4306-bab7-ab67f6b83d02";
+export default { extends: ruleset };
